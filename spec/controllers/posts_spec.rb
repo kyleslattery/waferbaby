beaedb367e6eb043e172916f6593c2031415d0d6
Daniel Bogan <d@waferbaby.com>
2008-06-16 11:39:36 +1000

    config/init.rb

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -1,9 +1,7 @@
 require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
 
 describe Posts, "index action" do
-
-        before(:each) do
-                dispatch_to(Posts, :index)
-        end
-
+  before(:each) do
+    dispatch_to(Posts, :index)
+  end
 end
