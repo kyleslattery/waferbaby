75e357914a7a9a14504cf10dd80b0e1a145edf47
Daniel Bogan <d@waferbaby.com>
2008-07-01 19:05:48 +1000

    Added the link_to_post and url_for_post methods.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,12 @@
 module Merb
-    
     module PostsHelper
-
+            
+            def link_to_post(post)
+                    link_to post.title + ('.'), url_for_post(post)
+            end
+            
+            def url_for_post(post)
+                    "/archives/#{post.created_on.year}/#{post.created_on.month}/#{post.created_on.day}/#{post.slug}"
+            end
     end
-    
 end
