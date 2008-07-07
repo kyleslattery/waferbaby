0712e90775f507a08e7b013cab3f88eff100b028
Daniel Bogan <d@waferbaby.com>
2008-07-07 20:37:27 +1000

    Added the index_by_date skeleton method.

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,7 +1,68 @@
 class Posts < Application
-  
+        provides :xml
+
         def index
+                @posts = Post.all
+                display @posts
+        end
+        
+        def index_by_date(year, month = nil, day = nil)
+        end
+        
+        def show
+                @post = Post.get()
+                raise NotFound unless @post
+                display @post
+        end
+
+        def show_by_slug(slug)
+                @post = Post.first(:slug => slug)
+                raise NotFound unless @post
+                
+                render :show
+        end        
+
+        def new
+                only_provides :html
+                @post = Post.new
                 render
         end
-  
+
+        def edit
+                only_provides :html
+                @post = Post.get()
+                raise NotFound unless @post
+                render
+        end
+
+        def create
+                @post = Post.new(params[:post])
+                if @post.save
+                        redirect url(:post, @post)
+                else
+                        render :new
+                end
+        end
+
+        def update
+                @post = Post.get()
+                raise NotFound unless @post
+                @post.attributes = params[:post]
+                if  @post.save
+                        redirect url(:post, @post)
+                else
+                        raise BadRequest
+                end
+        end
+
+        def destroy
+                @post = Post.get()
+                raise NotFound unless @post
+                if @post.destroy
+                        redirect url(:post)
+                else
+                        raise BadRequest
+                end
+        end
+
 end
