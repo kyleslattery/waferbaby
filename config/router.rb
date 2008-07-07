8a7bd48a75dee454344e39488dbf98f4fcff9c66
Daniel Bogan <d@waferbaby.com>
2008-07-07 20:39:59 +1000

    Added the individual post date routes.

--- config/router.rb
+++ config/router.rb
@@ -1,5 +1,9 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
-        r.match('/archives/:year/:month/:day/:slug').to(:controller => 'posts', :action => 'show_by_slug')
-        r.match('/').to(:controller => 'posts', :action => 'index')
+        r.match('/archives/:year/:month/:day/:slug').to(:controller => 'posts', :action => 'show_by_slug').name(:post)
+        r.match('/archives/:year/:month/:day').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_day)
+        r.match('/archives/:year/:month').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_month)
+        r.match('/archives/:year').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_year)        
+        
+        r.match('/').to(:controller => 'posts', :action => 'index').name(:posts)
 end
