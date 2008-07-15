d607cfb7320b92a47e472b57c5168556a901249e
Daniel Bogan <d@waferbaby.com>
2008-07-16 03:44:04 +1000

    Updated to use strftime everywhere. Added default values for the link string used in the link_to_* methods.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,23 +1,23 @@
 module Merb
         module PostsHelper
-                def link_to_post(post)
-                        link_to post.title + ('.'), url_for_post(post)
+                def link_to_post(post, title = post.title)
+                        link_to title, url_for_post(post)
                 end
                 
-                def link_to_post_year(post)
-                        link_to post.created_on.year, url_for_post_year(post)
+                def link_to_post_year(post, title = post.created_on.strftime('%Y'))
+                        link_to title, url_for_post_year(post)
                 end
                 
-                def link_to_post_month(post)
-                        link_to post.created_on.strftime('%m'), url_for_post_month(post)
+                def link_to_post_month(post, title = post.created_on.strftime('%m'))
+                        link_to title, url_for_post_month(post)
                 end
                 
-                def link_to_post_day(post)
-                        link_to post.created_on.day, url_for_post_day(post)
+                def link_to_post_day(post, title = post.created_on.strftime('%d'))
+                        link_to title, url_for_post_day(post)
                 end
 
                 def url_for_post_year(post)
-                        "/archives/#{post.created_on.year}"
+                        "/archives/#{post.created_on.strftime('%Y')}"
                 end
 
                 def url_for_post_month(post)
@@ -25,7 +25,7 @@ module Merb
                 end
                 
                 def url_for_post_day(post)
-                        url_for_post_month(post) + "/#{post.created_on.day}"
+                        url_for_post_month(post) + "/#{post.created_on.strftime('%d')}"
                 end
                 
                 def url_for_post(post)
