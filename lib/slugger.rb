6923e6c882fd6f95fd6dd043aad2a1bca5e7cfb8
Daniel Bogan <d@waferbaby.com>
2008-07-18 15:26:11 +1000

    Added the Waferbaby module.

--- lib/slugger.rb
+++ lib/slugger.rb
@@ -1,18 +1,20 @@
 require 'iconv'
 
-module Slug
-        def self.for(field)
-                field = Iconv.iconv('ascii//translit//IGNORE', 'utf-8', field).to_s
+module Waferbaby
+        module Slug
+                def self.for(field)
+                        field = Iconv.iconv('ascii//translit//IGNORE', 'utf-8', field).to_s
 
-                field.gsub!(/\W+/, ' ')
-                field.strip!
-                field.downcase!
-                field.gsub!(/\s+/, '-')
+                        field.gsub!(/\W+/, ' ')
+                        field.strip!
+                        field.downcase!
+                        field.gsub!(/\s+/, '-')
 
-                field
-        end
+                        field
+                end
         
-        def slug_for(field_symbol)
-                Slug.for(self.send(field_symbol))
+                def slug_for(field_symbol)
+                        Waferbaby::Slug.for(self.send(field_symbol))
+                end
         end
 end
