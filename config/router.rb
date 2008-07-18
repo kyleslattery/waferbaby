cc2d1e030bfa1c47fdef8358f9cb2af9385327f0
Daniel Bogan <d@waferbaby.com>
2008-07-19 00:38:04 +1000

    Added the about route.

--- config/router.rb
+++ config/router.rb
@@ -4,6 +4,6 @@ Merb::Router.prepare do |r|
         r.match('/archives/:year/:month/:day').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_day)
         r.match('/archives/:year/:month').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_month)
         r.match('/archives/:year').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_year)        
-        
+        r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
         r.match('/').to(:controller => 'posts', :action => 'index').name(:posts)
 end
