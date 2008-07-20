65006936d14d8519b86501a4d18ab284e75a5f8a
Daniel Bogan <d@waferbaby.com>
2008-07-20 22:02:13 +1000

    Removed the 'Waferbaby' module. Seems like overkill now.

--- lib/model_dates.rb
+++ lib/model_dates.rb
@@ -1,27 +1,25 @@
-module Waferbaby
-        module ModelDates
-                def created_year
-                        created_on.strftime('%Y')
-                end
+module ModelDates
+        def created_year
+                created_on.strftime('%Y')
+        end
 
-                def created_month
-                        created_on.strftime('%m')
-                end
+        def created_month
+                created_on.strftime('%m')
+        end
 
-                def created_day
-                        created_on.strftime('%d')
-                end
-        
-                def updated_year
-                        updated_on.strftime('%Y')
-                end
-        
-                def updated_month
-                        updated_on.strftime('%m')
-                end
-        
-                def updated_day
-                        updated_on.strftime('%d')
-                end
+        def created_day
+                created_on.strftime('%d')
+        end
+
+        def updated_year
+                updated_on.strftime('%Y')
+        end
+
+        def updated_month
+                updated_on.strftime('%m')
+        end
+
+        def updated_day
+                updated_on.strftime('%d')
         end
 end
--- lib/slugger.rb
+++ lib/slugger.rb
@@ -1,20 +1,18 @@
 require 'iconv'
 
-module Waferbaby
-        module Slug
-                def self.for(field)
-                        field = Iconv.iconv('ascii//translit//IGNORE', 'utf-8', field).to_s
+module Slug
+        def self.for(field)
+                field = Iconv.iconv('ascii//translit//IGNORE', 'utf-8', field).to_s
 
-                        field.gsub!(/\W+/, ' ')
-                        field.strip!
-                        field.downcase!
-                        field.gsub!(/\s+/, '-')
+                field.gsub!(/\W+/, ' ')
+                field.strip!
+                field.downcase!
+                field.gsub!(/\s+/, '-')
 
-                        field
-                end
-        
-                def slug_for(field_symbol)
-                        Waferbaby::Slug.for(self.send(field_symbol))
-                end
+                field
+        end
+
+        def slug_for(field_symbol)
+                Slug.for(self.send(field_symbol))
         end
 end
