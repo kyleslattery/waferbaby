64f654583db03f44ca546ca9ea6e1e74cb5356e0
Daniel Bogan <d@waferbaby.com>
2008-07-27 19:01:11 +1000

    Added the link_to_post_category method, and added titles to all the link_to_* methods.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,38 @@
 module Merb
         module PostsHelper
                 def link_to_post(post, title = post.title)
-                        link_to title, url_for_post(post)
+                        link_to(title,
+                                url_for_post(post),
+                                :title => "View the full '#{post.title}' post."
+                        )
                 end
                 
                 def link_to_post_year(post, title = post.created_year)
-                        link_to title, url_for_post_year(post)
+                        link_to(title,
+                                url_for_post_year(post),
+                                :title => "View all posts made in #{post.created_year}."
+                        )
                 end
                 
                 def link_to_post_month(post, title = post.created_month)
-                        link_to title, url_for_post_month(post)
+                        link_to(title,
+                                url_for_post_month(post),
+                                :title => "View all posts made on #{post.created_year}/#{post.created_month}."
+                        )
                 end
                 
                 def link_to_post_day(post, title = post.created_day)
-                        link_to title, url_for_post_day(post)
+                        link_to(title,
+                                url_for_post_day(post),
+                                :title => "View all posts made on #{post.created_year}/#{post.created_month}/#{post.created_day}."
+                        )
+                end
+                
+                def link_to_post_category(category)
+                        link_to(category.name,
+                                url(:post_category, :category => category.slug),
+                                :title => "View all posts filed under '#{category.name}'."
+                        )
                 end
 
                 def url_for_post_year(post)
@@ -40,5 +59,7 @@ module Merb
                         contents = post.is_selected ? post.contents : post.contents.first_paragraph
                         BlueCloth::new(contents).to_html
                 end
+                
+                
         end
 end
