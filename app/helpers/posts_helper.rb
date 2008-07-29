c41dcb10a55e0b9e420de7a90db419399b444d3e
Daniel Bogan <d@waferbaby.com>
2008-07-29 21:44:14 +1000

    Added the link_to_post_created_date method.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -12,6 +12,12 @@ module Merb
                         )
                 end
                 
+                def link_to_post_created_date(post)
+                        link_to_post_year(post) + '/' +
+                        link_to_post_month(post) + '/' +
+                        link_to_post_day(post)
+                end
+                
                 def link_to_post_year(post, title = post.created_year)
                         link_to(title,
                                 url_for_post_year(post),
@@ -61,16 +67,5 @@ module Merb
                 def url_for_post(post)
                         url_for_post_day(post) + "/#{post.slug}"
                 end
-                
-                def title_of(post)
-                        post.is_selected ? post.title : link_to_post(post)
-                end
-                
-                def contents_of(post)
-                        contents = post.is_selected ? post.contents : post.contents.first_paragraph
-                        BlueCloth::new(contents).to_html
-                end
-                
-                
         end
 end
