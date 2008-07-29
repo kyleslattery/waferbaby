3895d1f32bae2169847483f48ad950c405e7fde0
Daniel Bogan <d@waferbaby.com>
2008-07-29 21:42:37 +1000

    Added the header comment and the markup method.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,6 +1,12 @@
+#
+# =>    global_helper.rb
+# =>    Copyright (c) 2008 Daniel Bogan. http://waferbaby.com/
+#
+
 module Merb
-
         module GlobalHelpers
+                def markup(string)
+                        BlueCloth::new(string).to_html
+                end
         end
-
 end
