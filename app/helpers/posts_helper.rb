1c5dbd35f9aa7f6e0639cb61b5023a929ae441a7
Daniel Bogan <d@waferbaby.com>
2008-08-10 23:54:24 +1000

    Removed all the link_to_post_xxx methods, instead simplfying it all down to a link_to_post_date method. Added classes to all the links we produce.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,34 +8,16 @@ module Merb
                 def link_to_post(post, title = post.title)
                         link_to(title,
                                 url_for_post(post),
-                                :title => "View the full '#{post.title}' post."
+                                :title => "View the full '#{post.title}' post.",
+                                :class => 'title'
                         )
                 end
                 
-                def link_to_post_created_date(post)
-                        link_to_post_year(post) + '/' +
-                        link_to_post_month(post) + '/' +
-                        link_to_post_day(post)
-                end
-                
-                def link_to_post_year(post, title = post.created_year)
-                        link_to(title,
-                                url_for_post_year(post),
-                                :title => "View all posts made in #{post.created_year}."
-                        )
-                end
-                
-                def link_to_post_month(post, title = post.created_month)
-                        link_to(title,
-                                url_for_post_month(post),
-                                :title => "View all posts made on #{post.created_year}/#{post.created_month}."
-                        )
-                end
-                
-                def link_to_post_day(post, title = post.created_day)
+                def link_to_post_date(post, title = post.created_day)
                         link_to(title,
                                 url_for_post_day(post),
-                                :title => "View all posts made on #{post.created_year}/#{post.created_month}/#{post.created_day}."
+                                :title => "View all posts made on #{post.created_year}/#{post.created_month}/#{post.created_day}.",
+                                :class => 'date'
                         )
                 end
                 
@@ -46,7 +28,8 @@ module Merb
                                 else
                                         link_to(category.name,
                                                 url(:post_category, :category => category.slug),
-                                                :title => "View all posts filed under '#{category.name}'."
+                                                :title => "View all posts filed under '#{category.name}'.",
+                                                :class => 'tag'
                                         )                                
                                 end
                         end.join(', ')
