bc22b6adfa03b336e10195b54664056894508a3e
Daniel Bogan <d@waferbaby.com>
2008-08-12 19:28:07 +1000

    Added a route for different post formats.

--- config/router.rb
+++ config/router.rb
@@ -1,10 +1,15 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
+        r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
+        
         r.match('/archives/categories/:category').to(:controller => 'posts', :action => 'index_by_category').name(:post_category)
+        r.match('/archives/:year/:month/:day/:slug.:format').to(:controller => 'posts', :action => 'show_by_slug').name(:post_format)
         r.match('/archives/:year/:month/:day/:slug').to(:controller => 'posts', :action => 'show_by_slug').name(:post)
         r.match('/archives/:year/:month/:day').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_day)
         r.match('/archives/:year/:month').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_month)
         r.match('/archives/:year').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_year)        
-        r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
+        
+        r.match('/signup').to(:controller => 'people', :action => 'signup').name(:signup)
+        
         r.match('/').to(:controller => 'posts', :action => 'index').name(:posts)
 end
