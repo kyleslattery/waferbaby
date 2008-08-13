7231118b11dcdb07b8c71e58a4dd9f9cfd02305c
Daniel Bogan <d@waferbaby.com>
2008-08-13 20:41:00 +1000

    Removed.

--- app/parts/note_part.rb
+++
@@ -1,9 +0,0 @@
-class NotePart < Merb::PartController
-
-        def index
-                @notes = Note.all(:order => [:created_on.desc])
-                
-                display @notes
-        end
-
-end
