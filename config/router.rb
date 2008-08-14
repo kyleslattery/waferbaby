fb62938e92c5734e3b7e824b205d1f53038adcd4
Daniel Bogan <d@waferbaby.com>
2008-08-14 19:33:07 +1000

    Added the slice route.

--- config/router.rb
+++ config/router.rb
@@ -1,5 +1,7 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
+        r.add_slice(:MerbAuth, :path => '', :default_routes => false) 
+        
         r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
         
         r.match('/archives/categories/:category').to(:controller => 'posts', :action => 'index_by_category').name(:post_category)
