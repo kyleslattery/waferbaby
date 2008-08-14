2c16284e4b71acbb6a2d859c6e271fbd0936f430
Daniel Bogan <d@waferbaby.com>
2008-08-14 19:32:54 +1000

    Added the header comment, and the MerbAuth mixins.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,10 @@
+#
+# =>    person.rb
+# =>    Copyright (c) 2008 Daniel Bogan. http://waferbaby.com/
+#
+
 class Person
-  include DataMapper::Resource
-  
+        include DataMapper::Resource
+        include MerbAuth::Adapter::DataMapper 
+        include MerbAuth::Adapter::DataMapper::DefaultModelSetup
 end
