3553d4d1fe0e146963a205b2375b6b88c9e697d9
Daniel Bogan <d@waferbaby.com>
2008-08-16 21:47:13 +1000

    Added the display_errors_for method, because I don't like Merb's error display stuff.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -8,5 +8,12 @@ module Merb
                 def markup(string)
                         RDiscount.new(string).to_html
                 end
+                
+                def display_errors_for(object, error_class='errors')
+                        return if object.errors.length < 1
+                        
+                        messages = "\n", object.errors.full_messages.collect { |message| tag('li', message) }.join("\n"), "\n"                        
+                        tag('ul', messages, :class => error_class)
+                end
         end
 end
