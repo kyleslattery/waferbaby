cf4be3479acb8cad4b109c7e2f4566b6966c1f52
Daniel Bogan <d@waferbaby.com>
2008-08-19 22:32:42 +1000

    Updated to be a base class, removing specifics of the Weblog-type post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,26 +12,14 @@ class Post
         property :id,           Integer, :serial => true
         property :slug,         String
         property :title,        String
-        property :contents,     Text
+        property :contents,     Text, :lazy => false
+        property :type,         Discriminator
         property :created_on,   DateTime
         property :updated_on,   DateTime
         
         has n,                  :categories, :through => Resource
         
-        validates_present       :title
         validates_present       :contents
-        validates_is_unique     :slug
         
         attr_accessor           :is_selected
-
-        before :save do
-                if new_record?
-                        self.slug = Iconv.iconv('ascii//translit//IGNORE', 'utf-8', self.title).to_s
-                        
-                        self.slug.gsub!(/\W+/, ' ')
-                        self.slug.strip!
-                        self.slug.downcase!
-                        self.slug.gsub!(/\s+/, '-')
-                end
-        end        
 end
