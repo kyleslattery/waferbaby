d778127ed9852e9e168e06941c45f4e86400220d
Daniel Bogan <d@waferbaby.com>
2008-08-29 00:18:30 +1000

    Added the scrawls relationship.

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,4 +7,6 @@ class Person
         include DataMapper::Resource
         include MerbAuth::Adapter::DataMapper 
         include MerbAuth::Adapter::DataMapper::DefaultModelSetup
+        
+        has n, :scrawls
 end
