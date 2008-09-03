085725e5605ea7c4e87b8b0840f4c5729f16235a
Daniel Bogan <d@waferbaby.com>
2008-09-03 19:47:41 +1000

    Fleshed out the index method, and added the show method.

--- slices/merb-auth/app/controllers/users.rb
+++ slices/merb-auth/app/controllers/users.rb
@@ -1,7 +1,14 @@
 class MerbAuth::Users < MerbAuth::Application
 
         def index
-                render "TEST"
+                @people = MA[:user].all(:order => [:created_at.desc])
+                display @people
         end
-
+        
+        def show
+                @person = MA[:user].first(:login => params[:login])
+                raise NotFound unless @person
+                display @person
+        end
+        
 end
