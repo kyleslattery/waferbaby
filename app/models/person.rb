b1e1b4b66d1288b43b7e1e7fc90e147c637f8cdb
Daniel Bogan <d@waferbaby.com>
2008-09-03 22:56:29 +1000

    Added the DataMapper::Timestamp module, and the comments relationship.

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,8 +5,10 @@
 
 class Person
         include DataMapper::Resource
+        include DataMapper::Timestamp
         include MerbAuth::Adapter::DataMapper 
         include MerbAuth::Adapter::DataMapper::DefaultModelSetup
         
         has n, :scrawls
+        has n, :comments
 end
