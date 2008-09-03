327436071f20d3a37bd08ec9a1f891d878ea40fe
Daniel Bogan <d@waferbaby.com>
2008-09-03 21:57:34 +1000

    Changed the archive resource to use the created_x prefix for the date keys, and added the comments sub-resource.

--- config/router.rb
+++ config/router.rb
@@ -3,13 +3,16 @@ Merb::Router.prepare do |r|
         r.add_slice(:MerbAuth, :path => '', :default_routes => false)
 
         r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
-
-        r.match('/archives/categories/:category').to(:controller => 'posts', :action => 'index_by_category').name(:post_category)
-        r.match('/archives/:year').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_year)
-        r.match('/archives/:year/:month').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_month)
-        r.match('/archives/:year/:month/:day').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_day)
-        r.resources(:archives, :controller => 'posts', :keys => ['year', 'month', 'day', 'slug'])
-
+  
+        r.match('/archives/categories/:category').to(:controller => 'posts', :action => 'index_by_category').name(:archive_category)
+        r.match('/archives/:created_year').to(:controller => 'posts', :action => 'index_by_date').name(:archives_by_year)
+        r.match('/archives/:created_year/:created_month').to(:controller => 'posts', :action => 'index_by_date').name(:archives_by_month)
+        r.match('/archives/:created_year/:created_month/:created_day').to(:controller => 'posts', :action => 'index_by_date').name(:archives_by_day)
+        
+        r.resources(:archives, :controller => 'posts', :keys => ['created_year', 'created_month', 'created_day', 'slug']) do |posts|
+                posts.resources(:comments)
+        end
+        
         r.resources(:wallscrawl)
         
         r.match('/').to(:controller => 'posts', :action => 'index').name(:posts)
