f44cda6522a1e27c2db0e637f3f106649703e9c4
Daniel Bogan <d@waferbaby.com>
2008-09-05 19:30:35 +1000

    Reworked the date routes so that they're re-usable for any controller.

--- config/router.rb
+++ config/router.rb
@@ -5,18 +5,15 @@ Merb::Router.prepare do |r|
         r.match('/about').to(:controller => 'help', :action => 'show_about').name(:about)
   
         r.match('/posts/categories/:category').to(:controller => 'posts', :action => 'index_by_category').name(:post_category)
-        r.match('/posts/:created_year').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_year)
-        r.match('/posts/:created_year/:created_month').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_month)
-        r.match('/posts/:created_year/:created_month/:created_day').to(:controller => 'posts', :action => 'index_by_date').name(:posts_by_day)
+
+        r.match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]', :created_day => '[4]').name(:scrawls_by_day)
+        r.match(%r'/([a-z]+)/([0-9]{4,})/([0-9]{2,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]', :created_month => '[3]').name(:scrawls_by_month)
+        r.match(%r'/([a-z]+)/([0-9]{4,})').to(:controller => '[1]', :action => 'index_by_date', :created_year => '[2]').name(:scrawls_by_year)
         
         r.resources(:posts, :keys => ['created_year', 'created_month', 'created_day', 'slug']) do |posts|
                 posts.resources(:comments)
         end
 
-        r.match('/wallscrawl/:created_year').to(:controller => 'wallscrawl', :action => 'index_by_date').name(:scrawls_by_year)
-        r.match('/wallscrawl/:created_year/:created_month').to(:controller => 'wallscrawl', :action => 'index_by_date').name(:scrawls_by_month)
-        r.match('/wallscrawl/:created_year/:created_month/:created_day').to(:controller => 'wallscrawl', :action => 'index_by_date').name(:scrawls_by_day)
-        
         r.resources(:wallscrawl)
         
         r.match('/').to(:controller => 'posts', :action => 'index').name(:posts)
