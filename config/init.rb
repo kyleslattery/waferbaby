da629f38c10e23022807a2b504fa7cd9690c53b7
Daniel Bogan <d@waferbaby.com>
2008-09-17 19:58:02 +1000

    Removed the MerbAuth config settings, the merb-slices and merb-auth dependencies.

--- config/init.rb
+++ config/init.rb
@@ -1,43 +1,32 @@
+$KCODE = 'UTF8'
+
 Gem.clear_paths
 Gem.path.unshift(Merb.root / "gems")
 
+use_orm                         :datamapper
+use_test                        :rspec
+use_template_engine             :haml
+
 Merb::BootLoader.before_app_loads do
-        Merb::Slices::config[:merb_auth][:layout] = :application 
-        
-        MA[:use_activation]             = true
-        MA[:forgotten_password]         = true
-        MA[:from_email]                 = "nobody@waferbaby.com"
-        MA[:welcome_subject]            = "Your new waferbaby.com account"
-        
-        MA.add_routes do |r|
-                r.match('/people/:login').to(:controller => 'users', :action => 'show').name(:profile)
-        end
-        
         Merb::Mailer.config             = {:sendmail_path => '/usr/sbin/sendmail'}
         Merb::Mailer.delivery_method    = :sendmail
         
         Merb.add_mime_type(:atom, :to_atom, %w[application/atom+xml])
 end
 
-Merb::BootLoader.after_app_loads do
-end
+dependency      'dm-aggregates'
+dependency      'dm-timestamps'
+dependency      'dm-validations'
+dependency      'merb-action-args'
+dependency      'merb-assets'
+dependency      'merb-haml'
+dependency      'merb_helpers'
+dependency      'merb-mailer'
+dependency      'rdiscount'
+dependency      'uuid'
+dependency      'lib/timestamp_extensions'
 
 Merb::Config.use do |c|
         c[:session_secret_key]  = '1703bf9efcbb694541de2ce7c48b9406da71ac00'
         c[:session_store]       = 'datamapper'
 end
-
-use_orm         :datamapper
-use_test        :rspec
-
-dependency      'dm-validations'
-dependency      'dm-timestamps'
-dependency      'merb-action-args'
-dependency      'merb-assets'
-dependency      'merb-slices'
-dependency      'merb-auth'
-dependency      'merb-haml'
-dependency      'merb_helpers'
-dependency      'rdiscount'
-dependency      'uuid'
-dependency      'lib/timestamp_extensions'
