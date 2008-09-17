a516f92d5e8db5d61279649993e670abb96280c8
Daniel Bogan <d@waferbaby.com>
2008-09-17 19:55:00 +1000

    Added the authorisation methods.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,2 +1,34 @@
 class Application < Merb::Controller
+
+        protected                      
+                def logged_in?
+                        current_person != :false
+                end
+                
+                def current_person
+                        @current_person ||= (login_from_session || :false)
+                end
+
+                def current_person=(person)
+                        session[:person] = person.nil? ? nil : person.id
+                        @current_user = person
+                end
+                
+                def login_from_session
+                        self.current_person = Person.get(session[:person]) if session[:person]
+                end
+                
+                def login_required
+                        authorized? || throw(:halt, :access_denied)
+                end
+                
+                def authorized?
+                        logged_in?
+                end
+                
+                def access_denied
+                        session[:return_to] = request.uri
+                        redirect url(:login)
+                end
+                
 end
